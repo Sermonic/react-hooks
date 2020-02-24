cce4ea8996e899c2cdc4290414a8d85963c6db37
Vitalik <vitaliyshcherbanych@gmail.com>
2020-02-24 17:38:07 +0200

    Adding authentication page

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,14 @@ import { Switch, Route } from "react-router-dom";
 
 import GlobalFeed from "./pages/globalFeed";
 import Article from "./pages/article";
+import Authenticaton from "./pages/authentication";
 
 export default () => {
   return (
     <Switch>
       <Route path="/" component={GlobalFeed} exact />
+      <Route path="/login" component={Authenticaton} />
+      <Route path="/register" component={Authenticaton} />
       <Route path="/article/:slug" component={Article} />
     </Switch>
   );
