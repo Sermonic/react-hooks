b14d752e16a26eaf21dc8fc9813984b547e1a2a3
Vitalik <vitaliyshcherbanych@gmail.com>
2020-02-25 13:56:32 +0200

    Validation errors

--- src/pages/authentication/index.js
+++ src/pages/authentication/index.js
@@ -4,6 +4,7 @@ import { Link, Redirect } from 'react-router-dom';
 import useFetch from "../../hooks/useFetch";
 import useLocalStorage from "../../hooks/useLocalStorage";
 import {CurrentUserContext} from '../../contexts/currentUser';
+import BackendErrorMessages from '../../components/backendErrorMessages';
 
 const Authentication = props => {
   const isLogin = props.match.path === '/login';
@@ -15,11 +16,9 @@ const Authentication = props => {
   const [password, setPassword] = useState('');
   const [username, setUsername] = useState('');
   const [isSuccessfulSubmit, setIsSuccessfulSubmit] = useState(false);
-  const [{response, isLoading}, doFetch] = useFetch(apiUrl);
-  const [token, setToken] = useLocalStorage('token');
-  const [currentUserState, setCurrentUserState] = useContext(CurrentUserContext);
-
-  console.log('currentUserState:', currentUserState);
+  const [{response, isLoading, error}, doFetch] = useFetch(apiUrl);
+  const [, setToken] = useLocalStorage('token');
+  const [, setCurrentUserState] = useContext(CurrentUserContext);
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -44,7 +43,7 @@ const Authentication = props => {
       isLoading: false,
       currentUser: response.user
     }))
-  }, [response, setToken]);
+  }, [response, setToken, setCurrentUserState]);
 
   if (isSuccessfulSubmit) {
     return <Redirect to='/' />
@@ -60,6 +59,7 @@ const Authentication = props => {
               <Link to={descriptionLink}>{descriptionText}</Link>
             </p>
             <form onSubmit={handleSubmit}>
+              {error && <BackendErrorMessages backendErrors={error.errors} />}
               <fieldset>
                 {!isLogin && (
                   <fieldset className="form-group">
