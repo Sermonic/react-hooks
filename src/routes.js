a4b69bcd5f89758f725508857a2cc9816e7196a6
Vitalik <vitaliyshcherbanych@gmail.com>
2020-02-29 11:42:06 +0200

    Add editArticle page

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import { Switch, Route } from "react-router-dom";
 import GlobalFeed from "./pages/globalFeed";
 import Article from "./pages/article";
 import CreateArticle from "./pages/createArticle";
+import EditArticle from "./pages/editArticle";
 import Authentication from "./pages/authentication";
 import TagFeed from "./pages/tagFeed";
 import YourFeed from "./pages/yourFeed";
@@ -13,6 +14,7 @@ export default () => {
     <Switch>
       <Route path="/" component={GlobalFeed} exact />
       <Route path="/article/new" component={CreateArticle} />
+      <Route path="/articles/:slug/edit" component={EditArticle} />
       <Route path="/feed" component={YourFeed} />
       <Route path="/tags/:slug" component={TagFeed} />
       <Route path="/login" component={Authentication} />
